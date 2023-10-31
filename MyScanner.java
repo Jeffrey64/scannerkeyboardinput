c368de6df34aaab9d5e36eaee247ec4e7ce9bc77
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-10-31 11:43:44 -0700

    added few lines of code

--- MyScanner.java
+++ MyScanner.java
@@ -7,7 +7,10 @@ public class MyScanner
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-
+            if(scanner.hasNext()) {
+                String str = scanner.next();
+                System.out.println("alpha");
+            }
         }
     }
 

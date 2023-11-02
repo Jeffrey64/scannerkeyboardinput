9c9e43a6bd57985c738e681a35c92c30415aa085
Jeffrey Lagunas <jeffrey.lagunas@mrjaffesclass.com>
2023-11-02 11:44:18 -0700

    added couple lines of code

--- MyScanner.java
+++ MyScanner.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class MyScanner
 {
-
+    int i = 12;
     public void run() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             if(scanner.hasNext()) {
                 String str = scanner.next();
-                System.out.println("alpha");
+                System.out.println(str);
+                scanner.nextInt(i*3);
             }
         }
     }
